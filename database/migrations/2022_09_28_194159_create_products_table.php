40607dc000b56b7fa0465a5b7aa38fdb9882d038
paulvalencia92 <apvalencia92@gmail.com>
2022-09-28 15:28:05 -0500

    Creacion de migraciones y relacion de los modelos

--- database/migrations/2022_09_28_194159_create_products_table.php
+++ database/migrations/2022_09_28_194159_create_products_table.php
@@ -12,15 +12,11 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('nit')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('type', ['emisor', 'receptor']);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('stock');
+            $table->float('value');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
---
+++ database/migrations/2022_09_28_200536_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->unique();
+            $table->unsignedBigInteger('sender_id')->comment('emisor');
+            $table->unsignedBigInteger('receiver_id')->comment('receptor');
+            $table->float('iva')->default(0);
+            $table->json('items');
+            $table->decimal('total');
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
